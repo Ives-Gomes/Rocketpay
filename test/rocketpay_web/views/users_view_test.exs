193723d2b0390672ebefe2e38c65bf0db8f88ca3
Ives Moreira <ivesmoreira3@gmail.com>
2021-02-27 17:14:13 -0300

    Tests - Views

---
+++ test/rocketpay_web/views/users_view_test.exs
@@ -0,0 +1,38 @@
+defmodule RocketpayWeb.UsersViewTest do
+  use RocketpayWeb.ConnCase, async: true
+
+  import Phoenix.View
+
+  alias Rocketpay.{Account, User}
+  alias RocketpayWeb.UsersView
+
+  test "renders create.json" do
+    params = %{
+      name: "Ives",
+      password: "123456",
+      nickname: "IvesM",
+      email: "ives.gomes@email.com",
+      age: 19
+    }
+
+    {:ok, %User{id: user_id, account: %Account{id: account_id}} = user} =
+      Rocketpay.create_user(params)
+
+    response = render(UsersView, "create.json", user: user)
+
+    expected_response = %{
+      message: "User created",
+      user: %{
+        account: %{
+          balance: Decimal.new("0.00"),
+          id: account_id
+        },
+        id: user_id,
+        name: "Ives",
+        nickname: "IvesM"
+      }
+    }
+
+    assert expected_response == response
+  end
+end
